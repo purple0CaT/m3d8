18177d9e27f0a0766eebac1d414b1c5fcce8a890
Istvan <steven.ford.j@gmail.com>
2021-08-18 16:07:43 +0300

    Codeeee

--- js.js
+++ js.js
@@ -1,10 +1,21 @@
 let url = "https://striveschool-api.herokuapp.com/api/product/"
 let dataBase
+
+// listeners
+let homePage = document.getElementById('homePage')
+let backPage = document.getElementById('backOffice')
+function home(){
+    homePage.classList.remove('d-none')
+    backPage.classList.add('d-none')
+}
+function post() {
+    homePage.classList.add('d-none')
+    backPage.classList.remove('d-none')
+}
+// OnLOAD
 window.onload = ()=>{
     loadData()
 }
-
-
 
 const loadData = async ()=>{
     try {
@@ -16,10 +27,33 @@ const loadData = async ()=>{
         }
         );
         let data = await response.json();
-        console.log(response)
-      } catch (err) {
-        console.log(err);
-      }
-    };
+        dataBase = data
 
-    
+        loadCard(dataBase)
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+
+// CARD LOADING HOME PAGE
+
+function loadCard(datas) {
+    homePage.innerHTML =""
+    datas.forEach(elem => {
+        homePage.innerHTML +=`
+    <div class="col-6 col-md-3">
+        <div class="card h-100">
+            <img src="${elem.imageUrl}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${elem.name}</h5>
+                <p class="card-text">${elem.brand}</p>
+                <p class="card-text">${elem.description}</p>
+                <strong class="card-text">${elem.price}</strong>
+                </div>
+                <a href="#" class="btn btn-light">Buy</a>
+        </div>
+    </div>
+        `
+    });
+}
